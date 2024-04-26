68f77f7b2d241c36631900b21d3aa59aa2f724fb
VitaLiNaLi <vitailina5@gmail.com>
2024-04-26 14:04:00 +0300

    without log

--- accum.js
+++ accum.js
@@ -9,12 +9,3 @@ function groupPeopleByCity(people) {
 }
 
 module.exports = groupPeopleByCity;
-
-const arrEx = [
-  { name: "Alice", age: 30},
-  { name: "Bob", age: 25, city: "Los Angeles" },
-  { name: "Charlie", age: 35, city: "New York" },
-  { name: "David", age: 40, city: "Los Angeles" },
-];
-
-console.log(groupPeopleByCity(arrEx));
